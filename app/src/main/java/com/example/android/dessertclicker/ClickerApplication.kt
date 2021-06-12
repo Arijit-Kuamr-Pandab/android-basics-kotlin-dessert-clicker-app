5512848265d97490f534e172eeabcbc7391625ce
Arijitkumarpandab <arijitkumarpandab@gmail.com>
2021-06-12 20:44:14 +0530

    Initialized Timber library.

--- app/src/main/java/com/example/android/dessertclicker/ClickerApplication.kt
+++ app/src/main/java/com/example/android/dessertclicker/ClickerApplication.kt
@@ -1,6 +1,14 @@
 package com.example.android.dessertclicker
 
 import android.app.Application
+import timber.log.Timber
 
 class ClickerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Initialization of Timber library.
+        Timber.plant(Timber.DebugTree())
+    }
 }
